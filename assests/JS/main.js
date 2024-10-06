fa6bbb6c3d2346e15c153c984502e4a9c92330d0
Hasnat Mahbub <imhasnat00@gmail.com>
2024-10-06 11:27:29 +0600

    slider code updated

--- assests/JS/main.js
+++ assests/JS/main.js
@@ -15,3 +15,42 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 5,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 24,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    575: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    280: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+  },
+});
